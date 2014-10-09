e46ba1369f08eeaf98a6ef21585091c66c72b8b4
Takashi Kawachi <tkawachi@gmail.com>
2014-10-09 22:49:22 +0900

    Moved to scala_2.10 and scala_2.11

--- build.sbt
+++ build.sbt
@@ -6,7 +6,14 @@ scalaVersion := "2.11.2"
 
 crossScalaVersions := Seq("2.10.4", "2.11.2")
 
+scalacOptions ++= Seq("-deprecation")
+
 libraryDependencies += "org.scala-lang" % "scala-reflect" % scalaVersion.value
+
+unmanagedSourceDirectories in Compile <+= (scalaVersion, sourceDirectory in Compile) {
+  case (v, dir) if v startsWith "2.10" => dir / "scala_2.10"
+  case (v, dir) if v startsWith "2.11" => dir / "scala_2.11"
+}
 
 scalariformSettings
 
---
+++ src/main/scala_2.11/com/github/tkawachi/macros/LineMacro.scala
@@ -0,0 +1,32 @@
+package com.github.tkawachi.macros
+
+import java.io.File
+
+import scala.language.experimental.macros
+import scala.reflect.macros.blackbox.Context
+
+object LineMacro {
+  def LINE: Int = macro lineImpl
+
+  def lineImpl(c: Context): c.Expr[Int] = {
+    import c.universe._
+
+    val line = Literal(Constant(c.enclosingPosition.line))
+
+    c.Expr[Int](line)
+  }
+
+  def FILE: String = macro fileImpl
+
+  def fileImpl(c: Context): c.Expr[String] = {
+    import c.universe._
+
+    val absolute = c.enclosingPosition.source.file.file.toURI
+    val base = new File(".").toURI
+
+    val path = Literal(Constant(base.relativize(absolute).getPath))
+
+    c.Expr[String](path)
+  }
+
+}
